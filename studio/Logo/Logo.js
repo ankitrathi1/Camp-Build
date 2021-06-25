d9a8f374fdc05d06d9de057dac9aa1a7ddb3cefd
LL\ankrathi1 <ankit.rathi2@sapient.com>
2021-06-25 18:35:54 +0530

    campaign changes

--- studio/Logo/Logo.js
+++ studio/Logo/Logo.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import styles from './myLogo.css';
 
 const myLogo = () => (
-  <img src="/static/footer-logo.png" className={styles.myLogo} alt="Lion Queen"/>
+  <img src="/static/footer-logo.png" alt="Lion Queen"/>
 );
 
 export default myLogo;
---
+++ studio/schemas/objects/socialLink.js
@@ -0,0 +1,87 @@
+// 1. Import the SocialInput react component
+import { MdPhonelinkSetup } from 'react-icons/md'
+import SocialInput from '../../components/SocialInput'
+
+// 2. List of social the editor may choose from
+const social = [ 
+    { title: 'Facebook', value: 'facebook'},
+    { title: 'Twitter', value: 'twitter'},
+    { title: 'YouTube', value: 'youtube'},
+    { title: 'Instagram', value: 'instagram'},
+    { title: 'LinkedIn', value: 'linkedin'},
+    { title: 'Pinterest', value: 'pinterest'},
+    { title: 'Whatsapp', value: 'whatsapp'},
+    { title: 'Skype', value: 'skype'},
+    { title: 'Phone', value: 'phone'},
+    { title: 'SnapChat', value: 'snapchat'},
+    { title: 'Other', value: 'other'}
+]
+
+// 3. Validate function which is invoked on user input
+const verifyInput = socialLink => {
+  const {channelName, channelUrl} = socialLink
+  if (!channelName) {
+    return 'Please select a social media channel'
+  }
+  if (!channelUrl) {
+    return 'Enter channel URL'
+  }
+  //return mediaName < mediaLink ? true : `Let's open the store before we close it on ${social}, shall we?`
+  return true
+}
+
+export default {
+  name: 'socialLink',
+  title: 'Social Media Channel',
+  type: 'object',
+
+  // 4. Perform validation
+  validation: Rule => Rule.custom(verifyInput),
+
+  fields: [
+    {
+      // 5. Enable editors to input a string from a predefined list (social)
+      name: 'channelName',
+      title: 'Channel Name',
+      type: 'string',
+      options: {
+        list: social
+      },
+      validation: Rule => Rule.required(),
+      //inputComponent: SocialInput
+    },
+    {
+      // 7. Same time input as above, but assigned to a different field
+      name: 'channelIcon',
+      title: 'Channel icon URL',
+      type: 'url',
+      description: 'If not provided, default will be used',
+    },
+    {
+      // 7. Same time input as above, but assigned to a different field
+      name: 'iconAlt',
+      title: 'Icon Alt Text',
+      type: 'string',
+    },
+    {
+      // 7. Same time input as above, but assigned to a different field
+      name: 'channelUrl',
+      title: 'Channel URL',
+      type: 'url',
+    }
+  ],
+
+  // 8. Define how the socialLink object will render in the Studio 
+  preview: {
+    select: {
+      social: 'channelName',
+      mediaLink: 'channelUrl'
+    },
+    prepare({social, mediaLink}) {
+      return {
+        title: social,
+        subtitle: `${mediaLink}`
+      }
+    }
+  }
+}
