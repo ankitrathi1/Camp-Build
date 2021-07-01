671daaa3f519fac7dd89da3c1198ba47d0194d2d
LL\ankrathi1 <ankit.rathi2@sapient.com>
2021-07-01 14:39:21 +0530

    studio changes

--- studio/schemas/documents/socialChannel.js
+++ studio/schemas/documents/socialChannel.js
@@ -6,17 +6,11 @@ export default {
   title: "Social Channel",
   fields: [
     {
-        name: "title",
-        type: "string",
-        title: "Carousel Name",
-        description: "Name is used to identify the slider for page layout",
-    },
-    {
         name: "socialChannel",
         type: "array",
         title: "SOCIAL MEDIA CHANNELS",
         of: [{type: "socialLink"}]
-      },
+      }
   ],
   preview: {
     select: {
