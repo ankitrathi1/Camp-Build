103b705c33c8f47942e302f8c48e93e67b86ee81
LL\ankrathi1 <ankit.rathi2@sapient.com>
2021-07-28 16:28:46 +0530

    preview changes

---
+++ web/src/components/no-preview.js
@@ -0,0 +1,14 @@
+import * as styles from "./campaign.module.css";
+import React from "react";
+
+function NoPreview(props) {
+    console.log('no preview data', props);
+  return (
+      <div className="cw_no_preview">
+         <p>To preview any campaign please login to Studio first, <a href={props.url} target="_blank">Click Here</a></p>
+         
+      </div>
+  );
+}
+
+export default NoPreview;
