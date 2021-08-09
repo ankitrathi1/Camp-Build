286c9d85ec22ee7b1191928fbce1832e076c16f7
ankit rathi <ankit.rathi2@sapient.com>
2021-08-09 22:00:16 +0530

    one trust changes

---
+++ web/src/components/onetrust.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function onetrust() {
+    return (
+        <>
+            {/* OneTrust Cookies Consent Notice start for unilever-campaign.netlify.app */}
+            <script async type="text/javascript" src="https://cdn.cookielaw.org/consent/85f62e3a-1550-41d3-b34a-06f23c84631d-test/OtAutoBlock.js"></script>
+            <script async src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" data-document-language="true" type="text/javascript" charset="UTF-8" data-domain-script="85f62e3a-1550-41d3-b34a-06f23c84631d-test"></script>
+            {/* OneTrust Cookies Consent Notice end for unilever-campaign.netlify.app */}
+        </>
+    )
+}
