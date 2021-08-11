61f9ab3ffeef04281ed4764e5e51f4963dd02b32
Jayastpl <82571334+Jayastpl@users.noreply.github.com>
2021-08-11 10:30:47 +0530

    Update campaignjs

--- web/src/templates/campaign.js
+++ web/src/templates/campaign.js
@@ -141,7 +141,6 @@ const CampaignTemplate = (props) => {
       <script async type="text/javascript" src="https://cdn.cookielaw.org/consent/85f62e3a-1550-41d3-b34a-06f23c84631d-test/OtAutoBlock.js"></script>
       <script async src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" data-document-language="true" type="text/javascript" charset="UTF-8" data-domain-script="85f62e3a-1550-41d3-b34a-06f23c84631d-test"></script>
       {/* OneTrust Cookies Consent Notice end for unilever-campaign.netlify.app */}
-
       {/* <script src="/googleAnalytics.js"></script> */}
       <link rel="stylesheet" href={childCSS} />
       {campaign.content.style.styleFile && (
